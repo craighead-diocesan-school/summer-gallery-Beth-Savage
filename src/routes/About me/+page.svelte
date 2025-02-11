ad1fa92263ba06448aa977ba6448a948eedbe14d
Beth-Savage <145308916+Beth-Savage@users.noreply.github.com>
2025-02-11 14:45:28 +1300

    Created about me page

---
+++ src/routes/About me/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Header from '$lib/Header.svelte'
+    import Footer from '$lib/Footer.svelte'
+  </script>
+  
+  <Header />
+  <main>
+    <h2> 
+        About Me 
+    </h2>
+    <p>
+        "My name is Beth Savage I am a 16 year old year 12 student at Craighead Diocian school."
+    </p>
+  </main>
+  <Footer />
