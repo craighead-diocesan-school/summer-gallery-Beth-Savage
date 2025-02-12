ff8fd486f73f8629163477cff080154e1ef6f127
Beth-Savage <145308916+Beth-Savage@users.noreply.github.com>
2025-02-13 09:03:44 +1300

    Styled Nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -6,11 +6,15 @@
   <style>
     nav{
         background: rgb(1, 133, 98);
-        color:rgb(85, 255, 0);
         text-decoration: none;
-        text-align: center;
+        padding: 20px;
+    }
+    a{
+        text-decoration: none;
+        color: rgb(85, 255, 0);
+        margin-left:150px ;
     }
     nav a:hover{
-        color:blue
+        color:blue; 
     }
   </style>
