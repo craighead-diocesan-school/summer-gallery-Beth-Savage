e5dddfe4770894119197ccd6c9b30b12d1e18322
Beth-Savage <145308916+Beth-Savage@users.noreply.github.com>
2025-02-20 09:37:24 +1300

    Create Image.svelte

---
+++ src/lib/Image.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let src = "placeholder.png"
+    export let alt = ''
+    export let caption = ''
+</script>
+
+<figure>
+    <img src="{src}" alt="{alt}">
+    <figcaption> {caption} </figcaption>
+</figure>
