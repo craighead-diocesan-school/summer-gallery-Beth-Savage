768e55a06f028a4d4925ff38d043984e220dda21
Beth-Savage <145308916+Beth-Savage@users.noreply.github.com>
2025-02-21 09:52:46 +1300

    Formatting and adding content

--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -3,20 +3,33 @@
     import Footer from '$lib/Footer.svelte'
     import Nav from '$lib/Nav.svelte' 
     import Card from '$lib/Card.svelte'
+    import Image from '$lib/Image.svelte'
   </script>
   
-  <Header />
+  <Header title="Summer Gallery" subtitle="Images from summer" />
   <Nav />
   <main>
     <h2> 
         About Me 
     </h2>
-    <div class="columns is-8">
-      <div class="column-is-half"><img src= "Summer-camp.jpg" /></div>
-      <div class="column-is-half"> My name is Beth Savage. I am a 16 year old, year 12 student at Craighead Diocian school.I do ballet and Musical theatre at Timaru Dance Studio. In the summer holidays I went to a six day summer camp for ballet in Wellington.</div>
+    <div class="columns is-8 is-multiline">
+      <div class="column is-full">My name is Beth Savage. I am a 16 year old, year 12 student at Craighead Diocian school.I do ballet and Musical theatre at Timaru Dance Studio. I play the piano and violin, I play the violin with the South Canterbury Youth Orchestra, Craighead String group and a String Trio with my friends. I sing in the Craighead Chorale and Choir.</div>
+      <div class="column is-half"> <Image class="is-fullwidth" src= "Summer-camp.jpg"  alt= "The view of the lobby day one of RAD summer camp" caption = "RAD summer camp"/></div>
+      <div class="column is-half"> In the summer holidays I went to a six day summer camp for ballet in Wellington. We did 6 hours of dancing a day for 6 days straight with a mix of ballet, jazz and contemporary. I was there with my brother and one other girl from my studio. While I was there I made two new friends; Bianca and Amy-Lee. We were in the Intermediate/Advanced Foundation class together. At the end of the week we did a showcase for the parents. </div>
+      <div class="column is-half">On Christmas we spent the day with family. We had lunch at our Grandparents house. It was our Dogs first Christmas and we gave her a new toy wrapped in plain brown paper. Like many children she had more fun with the wrapping than the present. We took her with us when we went to have lunch and she was very unhappy about being left out. I got a ukulele so I spent most of the afternoon playing it for her.</div>
+      <div class="column is-half"> <Image class="is-fullwidth" src= "Clara-Christmas.jpg"  alt= "Our Dog Clara playing with the wrapping paper on Christmas day"/></div>
     </div>
    
     
   </main>
   <Card />
   <Footer />
+
+  <style>
+    .is-full{
+      margin: 2%;
+    }
+    main{
+      margin: 0.5%;
+    }
+  </style>
